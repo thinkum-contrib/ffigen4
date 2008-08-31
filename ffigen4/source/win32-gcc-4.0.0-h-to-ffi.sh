b06f2c27f5b63f6d155cf1adcde1f5f75ed925fe
gb <gb@df28d2ca-d578-4763-9ef7-37c90b4b449d>
2008-08-31 08:29:36 +0000

    set CFLAGS to include mingw, w32api directories.
    git-svn-id: http://svn.clozure.com/publicsvn/ffigen4/trunk@49 df28d2ca-d578-4763-9ef7-37c90b4b449d

--- ffigen4/source/win32-gcc-4.0.0-h-to-ffi.sh
+++ ffigen4/source/win32-gcc-4.0.0-h-to-ffi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FFIGEN_DIR=`dirname $0`/../ffigen
-CFLAGS="-isystem ${FFIGEN_DIR}/include -isystem /usr/include -isystem /usr/local/include -quiet -fffigen ${CFLAGS}"
+CFLAGS="-isystem ${FFIGEN_DIR}/include -isystem /usr/include/mingw -isystem /usr/include/w32api -isystem /usr/local/include -quiet -fffigen ${CFLAGS}"
 GEN=${FFIGEN_DIR}/bin/ffigen
 
 
